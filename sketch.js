e58f360b4551812a54c9fe8376c364d96165a038
blacklag <71304633+blacklag@users.noreply.github.com>
2020-12-01 11:47:10 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://blacklag.github.io/drity-frog/model.json';
 
 
 function preload() {
